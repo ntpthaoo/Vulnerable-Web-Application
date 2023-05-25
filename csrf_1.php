1752ce0715867dcd8fd88e7c53360113b6219172
ntpthaoo <ntpthao1906@gmail.com>
2023-05-25 22:55:03 +0700

    scanthis

--- csrf_1.php
+++ csrf_1.php
@@ -1,18 +1,5 @@
 <?php
 
-/*
-
-bWAPP, or a buggy web application, is a free and open source deliberately insecure web application.
-It helps security enthusiasts, developers and students to discover and to prevent web vulnerabilities.
-bWAPP covers all major known web vulnerabilities, including all risks from the OWASP Top 10 project!
-It is for security-testing and educational purposes only.
-
-Enjoy!
-
-Malik Mesellem
-Twitter: @MME_IT
-
-bWAPP is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License (http://creativecommons.org/licenses/by-nc-nd/4.0/). Copyright © 2014 MME BVBA. All rights reserved.
 
 */
 
@@ -180,27 +167,6 @@ if(isset($_REQUEST["action"]) && isset($_REQUEST["password_new"]) && isset($_REQ
 
 </header>    
 
-<div id="menu">
-      
-    <table>
-        
-        <tr>
-            
-            <td><a href="portal.php">Bugs</a></td>
-            <td><a href="password_change.php">Change Password</a></td>
-            <td><a href="user_extra.php">Create User</a></td>
-            <td><a href="security_level_set.php">Set Security Level</a></td>
-            <td><a href="reset.php" onclick="return confirm('All settings will be cleared. Are you sure?');">Reset</a></td>            
-            <td><a href="credits.php">Credits</a></td>
-            <td><a href="http://itsecgames.blogspot.com" target="_blank">Blog</a></td>
-            <td><a href="logout.php" onclick="return confirm('Are you sure you want to leave?');">Logout</a></td>
-            <td><font color="red">Welcome <?php if(isset($_SESSION["login"])){echo ucwords($_SESSION["login"]);}?></font></td>
-            
-        </tr>
-        
-    </table>   
-   
-</div> 
 
 <div id="main">
     
@@ -235,93 +201,12 @@ if(isset($_REQUEST["action"]) && isset($_REQUEST["password_new"]) && isset($_REQ
     </form>
 
     <br />
-    <?php    
+    </div>
+    
+    
 
-            echo $message;
+    
 
-            $link->close();
-
-    ?>
-    
-</div>
-    
-<div id="side">    
-    
-    <a href="http://twitter.com/MME_IT" target="blank_" class="button"><img src="./images/twitter.png"></a>
-    <a href="http://be.linkedin.com/in/malikmesellem" target="blank_" class="button"><img src="./images/linkedin.png"></a>
-    <a href="http://www.facebook.com/pages/MME-IT-Audits-Security/104153019664877" target="blank_" class="button"><img src="./images/facebook.png"></a>
-    <a href="http://itsecgames.blogspot.com" target="blank_" class="button"><img src="./images/blogger.png"></a>
-
-</div>     
-    
-<div id="disclaimer">
-          
-    <p>bWAPP is licensed under <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank"><img style="vertical-align:middle" src="./images/cc.png"></a> &copy; 2014 MME BVBA / Follow <a href="http://twitter.com/MME_IT" target="_blank">@MME_IT</a> on Twitter and ask for our cheat sheet, containing all solutions! / Need an exclusive <a href="http://www.mmebvba.com" target="_blank">training</a>?</p>
-   
-</div>
-    
-<div id="bee">
-    
-    <img src="./images/bee_1.png">
-    
-</div>
-    
-<div id="security_level">
-  
-    <form action="<?php echo($_SERVER["SCRIPT_NAME"]);?>" method="POST">
-        
-        <label>Set your security level:</label><br />
-        
-        <select name="security_level">
-            
-            <option value="0">low</option>
-            <option value="1">medium</option>
-            <option value="2">high</option> 
-            
-        </select>
-        
-        <button type="submit" name="form_security_level" value="submit">Set</button>
-        <font size="4">Current: <b><?php echo $security_level?></b></font>
-        
-    </form>   
-    
-</div>
-    
-<div id="bug">
-
-    <form action="<?php echo($_SERVER["SCRIPT_NAME"]);?>" method="POST">
-        
-        <label>Choose your bug:</label><br />
-        
-        <select name="bug">
-   
-<?php
-
-// Lists the options from the array 'bugs' (bugs.txt)
-foreach ($bugs as $key => $value)
-{
-    
-   $bug = explode(",", trim($value));
-   
-   // Debugging
-   // echo "key: " . $key;
-   // echo " value: " . $bug[0];
-   // echo " filename: " . $bug[1] . "<br />";
-   
-   echo "<option value='$key'>$bug[0]</option>";
- 
-}
-
-?>
-
-
-        </select>
-        
-        <button type="submit" name="form_bug" value="submit">Hack</button>
-        
-    </form>
-    
-</div>
       
 </body>
     
