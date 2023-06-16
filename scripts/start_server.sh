fb2f38836b8ba9e484cb59c8ece2a650fa3efa38
ntpthaoo <ntpthao1906@gmail.com>
2023-06-17 01:57:52 +0700

    h

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,4 +3,4 @@
 # cp -R /home/ubuntu/your/php/project/* /var/www/html/
 
 sudo systemctl start apache2 
-docker run -d --name mysql-container -p 3307:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes mysql:latest
+
